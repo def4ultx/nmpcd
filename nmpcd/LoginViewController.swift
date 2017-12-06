ad1d8c900cd32036cfc42f4cf3f447439c81ef2e
def4ultx <bonkungga@hotmail.com>
2017-12-06 22:11:50 +0700

    Add Firebase Authentication.

--- nmpcd/LoginViewController.swift
+++ nmpcd/LoginViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class LoginViewController: UIViewController {
 
-
+    var handle: AuthStateDidChangeListenerHandle!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,50 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func loginAction(_ sender: Any) {
+        if self.emailText.text == "" || self.passwordText.text == "" {
+            self.pushAlert(title: "Error", message: "Please enter an email and password.", action: "OK", style: .cancel)
+        } else {
+            Auth.auth().signIn(withEmail: self.emailText.text!, password: self.passwordText.text!) { (user, error) in
+                if error == nil {
+                    self.pushAlert(title: "Success", message: "Login successful.", action: "OK", style: .cancel)
+                } else {
+                    self.pushAlert(title: "Error", message: (error?.localizedDescription)!, action: "OK", style: .cancel)
+                }
+            }
+        }
+    }
+    
+    @IBAction func forgotAction(_ sender: Any) {
+        let alert = UIAlertController(title: "Password Reset", message: "Please enter your email.", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.text = ""
+            textField.keyboardType = UIKeyboardType.emailAddress
+        }
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            Auth.auth().sendPasswordReset(withEmail: (alert?.textFields![0].text)!, completion: { (error) in
+                var title: String
+                var message: String
+                if error != nil {
+                    title = "Error"
+                    message = (error?.localizedDescription)!
+                } else {
+                    title = "Success"
+                    message = "Password reset email sent."
+                }
+                self.pushAlert(title: title, message: message, action: "OK", style: .cancel)
+            })
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func backAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func pushAlert(title: String, message: String, action: String, style: UIAlertActionStyle) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: action, style: style, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
