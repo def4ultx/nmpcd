d52a12ff9bb74f9a59dd6576922c6bc042be97e9
fondfondd <fondoo_fondii.mc@hotmail.com>
2017-12-18 02:42:06 +0700

    edit Auto layout

--- nmpcd/AccountViewController.swift
+++ nmpcd/AccountViewController.swift
@@ -11,9 +11,10 @@ import Firebase
 import FirebaseAuth
 import FirebaseDatabase
 
-class AccountViewController: UIViewController {
+class AccountViewController: UIViewController ,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     var databaseRef: DatabaseReference!
+    @IBOutlet weak var addPhotoButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
     @IBOutlet weak var fullnameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
@@ -23,6 +24,7 @@ class AccountViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         AppUtility.roundConner(item: self.logoutButton, radius: 8)
+        
         // Do any additional setup after loading the view.
         self.databaseRef = Database.database().reference()
         Auth.auth().addStateDidChangeListener { (auth, user) in
@@ -62,6 +64,40 @@ class AccountViewController: UIViewController {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func chooseImage(){
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {
+            (action:UIAlertAction) in imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {
+            (action:UIAlertAction) in imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            addPhotoButton.setBackgroundImage(image, for: UIControlState.normal)
+            
+            
+        }else {
+            //Error message
+        }
+        self.dismiss(animated: true, completion: nil)
+        print("camera is dismissed or not")
     }
     
     @IBAction func backAction(_ sender: Any) {
